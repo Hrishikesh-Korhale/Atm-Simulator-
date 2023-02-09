f898536facaa9d5d11c90efb8595c18bb8369d65
Hrishi <hrishikorhale2002@gmail.com>
2023-02-09 12:02:12 +0530

    Added Buttons

--- Login.java
+++ Login.java
@@ -19,18 +19,47 @@ public class Login extends JFrame {
         add(welcome);
 
         JLabel Cardno = new JLabel("Card No:");
-        Cardno.setFont(new Font("Raleway", Font.BOLD,38));
-        Cardno.setBounds(150,120,400,100);
+        Cardno.setFont(new Font("Raleway", Font.BOLD,28));
+        Cardno.setBounds(150,120,150,100);
         add(Cardno);
 
+        JTextField CardTextField = new JTextField();
+        CardTextField.setBounds(300,160,250,30);
+        add(CardTextField);
+
         JLabel Pin = new JLabel("Pin:");
-        Pin.setFont(new Font("Raleway", Font.BOLD,38));
-        Pin.setBounds(150,200,400,100);
+        Pin.setFont(new Font("Raleway", Font.BOLD,28));
+        Pin.setBounds(150,200,150,100);
         add(Pin);
+
+        JPasswordField PasswordField = new JPasswordField();
+        PasswordField.setBounds(300,240,250,30);
+        add(PasswordField);
 
         setSize(800,480);
         setVisible(true);
         setLocation(300,100);
+
+        JButton Login = new JButton("Sign In");
+        Login.setBackground(Color.BLACK);
+        Login.setForeground(Color.WHITE);
+        Login.setBounds(300,300,100,30);
+        add(Login);
+
+        JButton ClearButton = new JButton("Clear");
+        ClearButton.setBackground(Color.BLACK);
+        ClearButton.setForeground(Color.WHITE);
+        ClearButton.setBounds(450,300,100,30);
+        add(ClearButton);
+
+        JButton SignupButton = new JButton("Sign Up");
+        SignupButton.setBackground(Color.BLACK);
+        SignupButton.setForeground(Color.WHITE);
+        SignupButton.setBounds(370,350,100,30);
+        add(SignupButton);
+
+
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public static void main(String [] args){
