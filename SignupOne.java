3b47e00ac5056e41bb1892137d512e7218e107c6
Hrishi <hrishikorhale2002@gmail.com>
2023-02-10 13:48:43 +0530

    Added all Labels

--- SignupOne.java
+++ SignupOne.java
@@ -1,4 +1,86 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 
-public class SignupOne extends JFrame {
+public class SignupOne extends JFrame  {
+    SignupOne(){
+        setLayout(null);
+        setTitle("Sign up Form one");
+        setSize(850,800);
+        setLocation(300,10);
+        setVisible(true);
+        getContentPane().setBackground(Color.WHITE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Random ran = new Random();
+        long random = Math.abs((ran.nextLong()%9000L)+1000L);
+
+        JLabel formno = new JLabel("Application form no "+random);
+        formno.setFont(new Font("Raleway",Font.BOLD,38));
+        formno.setBounds(200,20,600,40);
+        add(formno);
+
+        JLabel PersonalDetails = new JLabel("Page 1: Personal Details");
+        PersonalDetails.setFont(new Font("Raleway",Font.BOLD,22));
+        PersonalDetails.setBounds(290,80,400,30);
+        add(PersonalDetails);
+
+        JLabel name = new JLabel("Name:");
+        name.setFont(new Font("Raleway",Font.BOLD,20));
+        name.setBounds(100,140,100,30);
+        add(name);
+
+        JLabel fname = new JLabel("Father's Name:");
+        fname.setFont(new Font("Raleway",Font.BOLD,20));
+        fname.setBounds(100,190,150,30);
+        add(fname);
+
+        JLabel dob = new JLabel("Date of Birth:");
+        dob.setFont(new Font("Raleway",Font.BOLD,20));
+        dob.setBounds(100,240,150,30);
+        add(dob);
+
+        JLabel Gender = new JLabel("Gender:");
+        Gender.setFont(new Font("Raleway",Font.BOLD,20));
+        Gender.setBounds(100,290,150,30);
+        add(Gender);
+
+        JLabel email = new JLabel("Email Address:");
+        email.setFont(new Font("Raleway",Font.BOLD,20));
+        email.setBounds(100,340,150,30);
+        add(email);
+
+        JLabel maritalstatus = new JLabel("Marital Status:");
+        maritalstatus.setFont(new Font("Raleway",Font.BOLD,20));
+        maritalstatus.setBounds(100,390,150,30);
+        add(maritalstatus);
+
+        JLabel address = new JLabel("Address:");
+        address.setFont(new Font("Raleway",Font.BOLD,20));
+        address.setBounds(100,440,150,30);
+        add(address);
+
+        JLabel city = new JLabel("City:");
+        city.setFont(new Font("Raleway",Font.BOLD,20));
+        city.setBounds(100,490,150,30);
+        add(city);
+
+        JLabel state = new JLabel("State:");
+        state.setFont(new Font("Raleway",Font.BOLD,20));
+        state.setBounds(100,540,150,30);
+        add(state);
+
+        JLabel pincode = new JLabel("Pin Code:");
+        pincode.setFont(new Font("Raleway",Font.BOLD,20));
+        pincode.setBounds(100,590,150,30);
+        add(pincode);
+
+    }
+
+    public static void main(String [] args)
+    {
+        new SignupOne();
+    }
+
 
 }
